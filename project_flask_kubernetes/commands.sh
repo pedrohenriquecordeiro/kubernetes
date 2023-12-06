2d6978459ce7532403e3f4555d42df8da3222a7f
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2023-12-06 20:33:07 -0300

    Update commands.sh

--- project_flask_kubernetes/commands.sh
+++ project_flask_kubernetes/commands.sh
@@ -27,6 +27,7 @@ sudo usermod -aG docker $USER&& newgrp docker
 
 
 minikube start --driver=docker
+minikube ssh docker pull pedrojjesus/flask-kubernetes-project:latest
 kubectl create deployment page-test --image=pedrojjesus/flask-kubernetes-project
 kubectl get deployments
 kubectl expose deployment page-test --type=LoadBalancer --port=5000
