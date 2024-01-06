3b374d9d57ee0d102b2e1ab17157ebabc5376348
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2024-01-06 19:41:13 -0300

    Update commands.sh

--- project_flask_kubernetes/commands.sh
+++ project_flask_kubernetes/commands.sh
@@ -15,7 +15,9 @@ echo \
 $(lsb_release -cs) stable" |sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
 sudo apt-get update -y &&
 sudo apt install docker-ce docker-ce-cli containerd.io &&
-sudo usermod -aG docker $USER&& newgrp docker &&
+sudo usermod -aG docker $USER&& newgrp docker 
+
+
 sudo apt-get update -y &&
 sudo apt install -y conntrack &&
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &&
