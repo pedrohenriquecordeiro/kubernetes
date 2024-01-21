8047ef5e3f8cf7b32f415366393b7fad1ad983d9
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2024-01-21 16:28:26 -0300

    Update install_minikube.sh

--- compute_engine/install_minikube.sh
+++ compute_engine/install_minikube.sh
@@ -1,31 +1,30 @@
 https://docs.altinity.com/altinitykubernetesoperator/kubernetesinstallguide/minikubeonlinux/
 
 
-sudo apt update &&
-sudo apt upgrade -y &&
-sudo apt install ca-certificates curl gnupg wget apt-transport-https -y &&
-sudo install -m 0755 -d /etc/apt/keyrings &&
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
-sudo chmod a+r /etc/apt/keyrings/docker.gpg &&
-echo \
- "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
- "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-sudo apt update &&
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &&
-sudo usermod -aG docker $USER &&
-newgrp docker &&
+curl -LO 'https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl' &&
+curl -LO "https://dl.k8s.io/v1.22.0/bin/linux/amd64/kubectl.sha256" &&
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check &&
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
+kubectl version
+
+
+sudo apt-get update &&
+sudo apt-get install -y ca-certificates curl gnupg lsb-release &&
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
+sudo apt-get update &&
+sudo apt install docker-ce docker-ce-cli containerd.io &&
+sudo usermod -aG docker $USER&& newgrp docker
+
+sudo apt-get update &&
+sudo apt install conntrack &&
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &&
-sudo install minikube-linux-amd64 /usr/local/bin/minikube 
+sudo install minikube-linux-amd64 /usr/local/bin/minikube &&
+sudo sysctl net/netfilter/nf_conntrack_max=524288 &&
+minikube start && echo "ok: started minikube successfully" &&
+sudo chown -R $USER.$USER .kube &&
+sudo chown -R $USER.$USER .minikube
 
-minikube version
 
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl &&
-chmod +x kubectl &&
-sudo mv kubectl /usr/local/bin/ &&
-kubectl version -o yaml
-
-minikube start --driver=docker
 
 minikube status
