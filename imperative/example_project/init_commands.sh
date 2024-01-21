580825b591394482e227f0120fe24f96fa4800f6
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2024-01-21 17:31:07 -0300

    Update init_commands.sh

--- imperative/example_project/init_commands.sh
+++ imperative/example_project/init_commands.sh
@@ -1,15 +1,22 @@
+# inicia servidor minikube
 minikube start --driver=docker
 
+# cria o deployment
 minikube ssh docker pull pedrojjesus/flask-kubernetes-project:latest &&
 kubectl create deployment page-test --image=pedrojjesus/flask-kubernetes-project &&
 kubectl get deployments
 
+# cria o service e link com o deployment criado
 kubectl expose deployment page-test --type=LoadBalancer --port=5000 &&
 kubectl get services
 
+## expoe o service no servido minikube
 minikube service page-test &&
 minikube tunnel
 
+## check do service executando
+curl http://192.168.49.2:30257 # url do service
+
+## logs
 kubectl logs -f <pod-id>
 
-curl http://192.168.49.2:30257 # url do service
