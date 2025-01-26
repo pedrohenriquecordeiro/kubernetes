f18dc0c4e0162b0de82d1ea4fd6c6b4682181043
Pedro Jesus <pedro.henrique.cefetmg@gmail.com>
2025-01-26 16:36:46 -0300

    Update config_cluster_minikube.sh

--- infra/gcp/compute_engine/config_cluster_minikube.sh
+++ infra/gcp/compute_engine/config_cluster_minikube.sh
@@ -1,10 +1,10 @@
 # INSTALL DOCKER 
 sudo apt update &&
-sudo apt install apt-transport-https ca-certificates curl software-properties-common &&
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y &&
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
 apt-cache policy docker-ce &&
-sudo apt install docker-ce &&
+sudo apt install docker-ce -y &&
 # sudo systemctl status docker &&
 sudo usermod -aG docker $USER && 
 newgrp docker &&
