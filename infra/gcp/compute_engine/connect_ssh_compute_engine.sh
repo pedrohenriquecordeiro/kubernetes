a4e35f1d4cd1a4339ac623eb34956c2dc5c0cf02
Pedro Jesus <pedro.henrique.cefetmg@gmail.com>
2025-11-28 09:27:23 -0300

    Update SSH config command to remove existing entry
    # make sure that add your current external ip in firewall allowlist

--- infra/gcp/compute_engine/connect_ssh_compute_engine.sh
+++ infra/gcp/compute_engine/connect_ssh_compute_engine.sh
@@ -1,1 +1,2 @@
-rm .ssh/google_compute_engine && gcloud compute config-ssh 
+rm .ssh/google_compute_engine && gcloud compute config-ssh --remove && gcloud compute config-ssh 
+# make sure that add your current external ip in firewall allowlist
